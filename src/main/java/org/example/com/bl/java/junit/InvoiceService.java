dccc630785ce9657c3413650db47be006f6083e6
Rehan Pinjari <pinjarirehan11@gmail.com>
2024-09-25 16:35:49 +0530

    Added: Welcome Message is added

---
+++ src/main/java/org/example/com/bl/java/junit/InvoiceService.java
@@ -0,0 +1,9 @@
+package org.example.com.bl.java.junit;
+
+public class InvoiceService {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Cab Invoice Generator Program");
+    }
+}
---
+++ src/test/java/org/example/com/bl/java/junit/InvoiceServiceTest.java
@@ -0,0 +1,10 @@
+package org.example.com.bl.java.junit;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InvoiceServiceTest {
+
+
+}
