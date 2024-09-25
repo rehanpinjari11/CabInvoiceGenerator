3a6bfeab2d0d33693576f22f781f92bd0440642f
Rehan Pinjari <pinjarirehan11@gmail.com>
2024-09-25 17:29:49 +0530

    Added: Minimum Fare is added

--- src/test/java/org/example/com/bl/java/junit/InvoiceServiceTest.java
+++ src/test/java/org/example/com/bl/java/junit/InvoiceServiceTest.java
@@ -17,4 +17,14 @@ public class InvoiceServiceTest {
         Assert.assertEquals(25, fare, 0.0);
     }
 
+    @Test
+    public void givenLessDistanceOrTime_ShouldReturnMinFare() {
+
+        InvoiceService invoiceService = new InvoiceService();
+        double distance = 0.1;
+        int time = 1;
+        double fare = invoiceService.calculateFare(distance, time);
+        Assert.assertEquals(5, fare, 0.0);
+    }
+
 }
