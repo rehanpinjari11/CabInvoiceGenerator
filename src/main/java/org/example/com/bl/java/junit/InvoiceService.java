0a99e599377a2266911081a200c7b5c85707812e
Rehan Pinjari <pinjarirehan11@gmail.com>
2024-09-26 13:56:53 +0530

    Added: Invoice Service is added

--- src/main/java/org/example/com/bl/java/junit/InvoiceService.java
+++ src/main/java/org/example/com/bl/java/junit/InvoiceService.java
@@ -5,6 +5,7 @@ public class InvoiceService {
     private static final int COST_PER_TIME = 1;
     private static final double MIN_COST_PER_KM = 10;
     private static final double MIN_FARE = 5;
+    private RideRepository rideRepository;
 
     public double calculateFare(double distance, int time) {
         double totalFare = distance *   MIN_COST_PER_KM + time * COST_PER_TIME;
@@ -20,5 +21,21 @@ public class InvoiceService {
         }
 
         return new InvoiceSummary(rides.length, totalFare);
+    }
+
+    public  InvoiceSummary calculateFareDescription(Ride[] rides) {
+        double cost = 0;
+        for (Ride ride : rides) {
+            cost = cost + this.calculateFare(ride.distance, ride.time);
+        }
+        return new InvoiceSummary(rides.length, cost);
+    }
+
+    public InvoiceSummary getInvoiceDescription(String userId) {
+        return this.calculateFareDescription(rideRepository.getRides(userId));
+    }
+
+    public void addRides(String userId, Ride[] rides) {
+        rideRepository.addRides(userId, rides);
     }
 }
